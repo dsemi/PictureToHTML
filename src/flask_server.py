cbafa1ec1622cc2a31d1631e42802b98c396627b
Dan Seminara <semi225599@gmail.com>
2014-02-23 05:15:47 -0500

    upload folder

--- src/flask_server.py
+++ src/flask_server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = ''
+UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
