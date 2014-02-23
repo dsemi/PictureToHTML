f66c5f48b34c31bac72649754a8415008e76ea6e
Dan Seminara <semi225599@gmail.com>
2014-02-23 00:29:28 -0500

    added main to char recog

--- src/ocr/character_recog.py
+++ src/ocr/character_recog.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python2
 from SimpleCV import Image
 
-img = Image('good_test.jpg')
-img = img.resize(img.width/2, img.height/2)
-img = img.grayscale().edges().dilate(4).erode()
-blobs = img.findBlobs()
-img.show()
-for b in blobs:
-    b.show()
-    raw_input()
+def main():
+    img = Image('good_test.jpg')
+    img = img.resize(img.width/2, img.height/2)
+    img = img.grayscale().edges().dilate(4).erode()
+    blobs = img.findBlobs()
+    img.show()
+    for b in blobs:
+        b.show()
+        raw_input()
     
+if __name__ == '__main__':
+    main()
