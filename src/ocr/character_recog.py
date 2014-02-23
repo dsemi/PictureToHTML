bae629141d2e93f2309d3495810543ad029bc8d3
Dan Seminara <semi225599@gmail.com>
2014-02-23 01:16:17 -0500

    command line option is more repeatable

--- src/ocr/character_recog.py
+++ src/ocr/character_recog.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python2
+import sys
 from SimpleCV import Image
 
 def main():
-    im_name = raw_input('Please enter image name: ')
-    img = Image(im_name)
+    img = Image(sys.argv[1])
     img = img.resize(img.width/2, img.height/2)
     img = img.grayscale().edges().dilate(4).erode()
     blobs = img.findBlobs()
     img.show()
     for b in blobs:
+        b = b.crop()
         b.show()
         raw_input()
     
