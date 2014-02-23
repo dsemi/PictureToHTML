6229d060da928b96f3147f9756116a9f20347557
Dan Seminara <semi225599@gmail.com>
2014-02-23 03:59:11 -0500

    email and dicts

--- src/html_structure/HTMLGenerator.py
+++ src/html_structure/HTMLGenerator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 def divs_from_boxes(box_list):
-    return '<html><style> .outlined { border: 1px solid black;  } </style>\n%s\n</html>' % ('\n'.join(['<div class="outlined" style="position: absolute; left: %d; top: %d; width: %d; height: %d;">\n</div>' % box for box in box_list]))
+    return '<html><style> .outlined { border: 1px solid black;  } </style>\n%s\n</html>' % ('\n'.join(['<div class="outlined" style="position: absolute; left: {x}; top: {y}; width: {width}; height: {height};">\n</div>'.format(**box) for box in box_list]))
     
 def main():
     pass
---
+++ src/sg_email/sg_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+
+import os
+import sendgrid
+
+
+def main():
+    s = sendgrid.SendGridClient(os.environ['SENDGRID_USERNAME'], os.environ['SENDGRID_KEY'], secure=True)
+
+    message = sendgrid.Mail(from_email='', subject='', text="", to=[""])
+    
+    # Attachments must be under 7MB in size each, and under 20MB total
+    # Should add a check eventually
+    # for filepath in args.attachments:
+        # message.add_attachment(os.path.basename(filepath), filepath)
+    s.send(message)
+
+if __name__ == '__main__':
+    main()
