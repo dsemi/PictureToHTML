cd1e9652fe8bb38090a0afa620129f291da77967
Kenny Ruddick <ruddick1@tcnj.edu>
2014-02-23 02:52:23 -0500

    Refined the DIV recognition

--- src/html_structure/converter.py
+++ src/html_structure/converter.py
@@ -3,34 +3,43 @@ from SimpleCV import Image
 
 
 def main():
+    RECT_TOL = 0.5
+    MIN_AREA = 500
+    NEAREST_EDGE = 20
+    MAX_HEIGHT = 700.0
+    
+  
     image = Image(sys.argv[1])
+    
+    # Resize Image
+    f = MAX_HEIGHT / image.height
+    image = image.resize(int(image.width * f), int(image.height * f))
 
     # Find corners to boxes
-    #image.grayscale().edges().show()
-    image = image.resize(image.width/2, image.height/2)
     image = image.grayscale().edges()
     image = image.smooth().dilate().erode()
     image = image.smooth().dilate().erode()
+    image = image.binarize()
 
-    blobs = image.binarize().findBlobs()
+    blobs = image.findBlobs()
 
     # Gets all the boxes in the image
-    boxes = [b.minRect() for b in blobs if b.area() > 200]
+    boxes = [b.minRect() for b in blobs if 
+	     b.isRectangle(RECT_TOL) 
+	     and b.area() > MIN_AREA 
+	     and b.distanceToNearestEdge() > NEAREST_EDGE]
 
     # Draw all the boxes
     for b in boxes:
         (x,y) = map(min, zip(*b))
-        (xmax,ymax) = map(max, zip(*b))
+        (xmax,ymax) = map(magitx, zip(*b))
         
         width = xmax - x
         height = ymax - y
   
-        print(str.format('Origin: ({0}, {1})\nWidth: {2}\nHeight: {3}\n', x, y, width, height))
-        image.drawRectangle(x, y, width, height)
+	image.drawRectangle(x, y, width, height)
   
     image.show()
-  
-    raw_input()
   
 if __name__ == '__main__':
     main()
